f76f4d95d984e2170076d076bd0bfaf353e8d6d1
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-07-06 23:37:09 -0700

    Add paginate component and propTypes fro debugging

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
@@ -11,8 +13,11 @@ const Pagination = props => {
     <nav>
       <ul className="pagination justify-content-center">
         {pages.map(page => (
-          <li key={page} className="page-item">
-            <a className="page-link" onClick={() => props.onPageChange(page)}>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item-active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
@@ -20,6 +25,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
