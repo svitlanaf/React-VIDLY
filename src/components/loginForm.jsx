508be33da9b7d357cf4748ef5f3efebbbf37f12a
Student <example@example.com>
2019-07-08 14:15:21 -0700

    Add dislaying validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -23,11 +23,8 @@ class LoginForm extends Component {
         event.preventDefault();
 
         const errors = this.validate();
-        console.log(errors);
-        this.setState({ errors });
+        this.setState({ errors: errors || {} });
         if(errors) return;
-
-        // const username = this.username.current.value;
         console.log('SUbmitted');
     };
 
@@ -38,14 +35,26 @@ class LoginForm extends Component {
     };
 
     render() {
-        const {account} = this.state;
+        const {account, errors} = this.state;
 
         return (
         <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-                <Input name="username" value={account.username} label="Username" onChange={this.handleChange} />
-                <Input name="password" value={account.password} label="Password" onChange={this.handleChange}/>
+                <Input 
+                name="username" 
+                value={account.username} 
+                label="Username" 
+                onChange={this.handleChange} 
+                error={errors.username}
+                />
+                <Input 
+                name="password" 
+                value={account.password} 
+                label="Password" 
+                onChange={this.handleChange}
+                error={errors.password}
+                />
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
         </div>
